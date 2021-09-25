8056498de53f33d57bd8bc06ae23e5967c1532f9
Kieran Hall <kieran.hall@gmail.com>
2021-09-25 18:44:45 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # dusk-eslint-config
-ESLint config use by Dusk Javascript apps
+
+> ESLint config use by Dusk Javascript apps.
+
+## Installation
+
+```bash
+npm i -D eslint @dusk/eslint-config
+```
+
+## Usage
+
+```json
+  "eslintConfig": {
+    "extends": "@svelterialjs"
+  }
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+    root: true,
+    extends: ["eslint:recommended", "prettier", "plugin:tailwind/recommended"],
+    plugins: ["svelte3"],
+    overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+    parserOptions: {
+      sourceType: "module",
+      ecmaVersion: 2020,
+    },
+    globals: {
+      window: true,
+      module: true,
+    },
+    env: {
+      browser: true,
+      es6: true,
+      node: true,
+    },
+    settings: {
+      "svelte3/ignore-styles": () => true,
+    },
+  };
+  
