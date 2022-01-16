fb79ca863a9ef37cfd364942054ff5b7f93402cf
Kieran Hall <kieran.hall@gmail.com>
2022-01-16 12:45:07 +0100

    Update to to use new tailwind plugin

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     root: true,
-    extends: ["eslint:recommended", "prettier", "plugin:tailwind/recommended"],
+    extends: ["eslint:recommended", "prettier", "plugin:tailwindcss/recommended"],
     plugins: ["svelte3"],
     overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
     parserOptions: {
