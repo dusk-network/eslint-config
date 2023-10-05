f2892f76003f19277b8916a69c37abd1817e6556
Kieran Hall <kieran@dusk.network>
2023-10-05 16:43:43 +0200

    Enforce whitespace before self-closing tag end (#9)
    Add svelte/html-closing-bracket-spacing rule that enforces white space before the end of the tag, which is inline with Prettier's opinionated (non-configurable) formatting.
    
    Resolves #8

--- svelte/base.js
+++ svelte/base.js
@@ -1,10 +1,17 @@
 module.exports = {
 	rules: {
 		"svelte/button-has-type": [
-			"error", {
-				"button": true,
-				"reset": true,
-				"submit": true
+			"error",
+			{
+				button: true,
+				reset: true,
+				submit: true
+			}
+		],
+		"svelte/html-closing-bracket-spacing": [
+			"error",
+			{
+				selfClosingTag: "always"
 			}
 		],
 		"svelte/infinite-reactive-loop": "error",
@@ -24,17 +31,19 @@ module.exports = {
 		"svelte/no-shorthand-style-property-overrides": "error",
 		"svelte/no-store-async": "error",
 		"svelte/no-target-blank": [
-			"error", {
-				"allowReferrer": false,
-				"enforceDynamicLinks": "always"
+			"error",
+			{
+				allowReferrer: false,
+				enforceDynamicLinks: "always"
 			}
 		],
 		"svelte/no-unknown-style-directive-property": "error",
 		"svelte/no-unused-svelte-ignore": "error",
 		"svelte/no-useless-mustaches": [
-			"error", {
-				"ignoreIncludesComment": true,
-				"ignoreStringEscape": true
+			"error",
+			{
+				ignoreIncludesComment: true,
+				ignoreStringEscape: true
 			}
 		],
 		"svelte/prefer-destructured-store-props": "error",
@@ -43,10 +52,7 @@ module.exports = {
 		"svelte/require-store-callbacks-use-set-param": "error",
 		"svelte/require-store-reactive-access": "error",
 		"svelte/require-stores-init": "error",
-		"svelte/valid-compile": [
-			"error",
-			{ "ignoreWarnings": false }
-		],
+		"svelte/valid-compile": ["error", { ignoreWarnings: false }],
 		"svelte/valid-each-key": "error"
 	}
 };
