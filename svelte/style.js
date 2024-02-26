6df6044615fa48fab53fc48b1a6216b31927d25c
Kieran Hall <kieran@dusk.network>
2024-02-26 23:36:59 +0100

    Update config for release 3.0.2
    Resolves #26

--- svelte/style.js
+++ svelte/style.js
@@ -5,39 +5,6 @@ module.exports = {
       { reportUnusedDisableDirectives: true },
     ],
     "svelte/derived-has-same-inputs-outputs": "error",
-    "svelte/first-attribute-linebreak": [
-      "error",
-      {
-        multiline: "below",
-        singleline: "beside",
-      },
-    ],
-    "svelte/html-closing-bracket-spacing": [
-      "error",
-      {
-        endTag: "never",
-        selfClosingTag: "never",
-        startTag: "never",
-      },
-    ],
-    "svelte/html-quotes": [
-      "error",
-      {
-        dynamic: {
-          avoidInvalidUnquotedInHTML: false,
-          quoted: false,
-        },
-        prefer: "double",
-      },
-    ],
-    "svelte/indent": ["error", { indent: "tab", switchCase: 1 }],
-    "svelte/max-attributes-per-line": [
-      "error",
-      {
-        multiline: 1,
-        singleline: 3,
-      },
-    ],
     "svelte/no-restricted-html-elements": [
       "error",
       {
@@ -62,7 +29,6 @@ module.exports = {
         message: "Do not use deprecated HTML tags",
       },
     ],
-    "svelte/no-spaces-around-equal-signs-in-attribute": "error",
     "svelte/spaced-html-comment": ["error", "always"],
   },
 };
